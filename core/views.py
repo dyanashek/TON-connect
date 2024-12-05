fa0a1fe7783f4b023aa60f233d0fec96bd32ac8e
Ianashek Denis <yanashek@gmail.com>
2024-12-05 15:04:22 +0700

    fix

--- core/views.py
+++ core/views.py
@@ -4,12 +4,10 @@ from django.views import View
 
 class TonConnectManifestView(View):
     def get(self, request, *args, **kwargs):
-        # Путь к вашему манифесту
         manifest = {
             "name": "My TON App",
-            "url": "https://ton-auth-dyanashek.amvera.io/",
+            "url": "https://ton-auth-dyanashek.amvera.io",
             "iconUrl": "https://w7.pngwing.com/pngs/289/640/png-transparent-computer-icons-ios-7-ios-rectangle-waste-magenta-thumbnail.png"
         }
 
-        # Возвращаем JSON-ответ с манифестом
         return JsonResponse(manifest)
