e37adc2abd9ad868c21a46695d0d9596a330ef77
Ianashek Denis <yanashek@gmail.com>
2024-12-19 16:07:23 +0700

    fix

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ app_name = 'core'
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html'), name='loader'),
     path('profile/', TemplateView.as_view(template_name='profile.html'), name='profile'),
+    path('testing/', TemplateView.as_view(template_name='testing.html'), name='testing'),
     path('solana/', TemplateView.as_view(template_name='solana.html'), name='solana'),
     path('tonconnect-manifest.json', core_views.TonConnectManifestView.as_view(), name='tonconnect_manifest'),
     ]
